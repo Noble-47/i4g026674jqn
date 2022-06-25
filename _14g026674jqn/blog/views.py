3b89b81ffc37c12a052dc31e2596c3c5365cd660
noble-47 <noblebest56@gmail.com>
2022-06-25 21:35:37 +0100

    created views and added templates to project

--- _14g026674jqn/blog/views.py
+++ _14g026674jqn/blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
