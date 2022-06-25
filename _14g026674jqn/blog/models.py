a67dfec29ea5e71874a0df77ad1c5df84f78f29c
noble-47 <noblebest56@gmail.com>
2022-06-25 21:30:20 +0100

    Updated models

--- _14g026674jqn/blog/models.py
+++ _14g026674jqn/blog/models.py
@@ -1,21 +1,44 @@
+from django.template.defaultfilters import slugify
 from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.urls import reverse
+from django.db import models
 from django.db import models
 
-"""
- Post
- --------
-Title : A string of maxlength 200, use Django’s models.CharField
-Text : Any amount of text, use Django’s TextField
-Author : A Foreign Key to the current user model. Make use of Django’s get_user_model function.
-Created_date : A date-time column, use Django’s models.DateTimeField.
-Published_date : A date-time column, use Django’s models.DateTimeField.
-"""
+# Create your models here.
+
 User = get_user_model()
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
-    Created_date = models.DateTimeField(auto_now_add=True)
-    Published_date = models.DateTimeField()
+
+    STATUS_CHOICES = (("draft", "Draft"), ("published", "Published"))
+
+    # DB Fields
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=300, unique=True, editable=False)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        pass
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"slug": self.slug})
